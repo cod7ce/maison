11be677587422b41e062fb0d9340b55587655abe
cod7ce <cod7ce@gmail.com>
2014-02-02 21:04:05 +0800

    壁纸json数据中的page属性变为int类型

--- app/controllers/wallpapers_controller.rb
+++ app/controllers/wallpapers_controller.rb
@@ -1,6 +1,6 @@
 class WallpapersController < ApplicationController
   def index
-    @page = params[:page] ? params[:page] : '1';
+    @page = params[:page] ? params[:page].try('to_i') : 1;
     @count = Wallpaper.count
     @wallpapers = Wallpaper.page(@page).per(1)
   end
